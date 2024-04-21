d34965bc8b2059cef9566f6b0d3dbd4751cb0bcf
LucasMonteiroS <lucas.silva@novaandradina.org>
2024-04-21 19:50:17 -0400

    First Updates

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,32 @@
 import { Link } from 'react-router-dom'
+import { BsCart4 } from "react-icons/bs";
 import './style.css'
+import { useEffect, useState } from 'react';
+
+interface UserLogged {
+    uid: string;
+    email: string;
+}
 export function Header(){
+    const [userData, setUserData] = useState<UserLogged | null>(null)
+
+    useEffect( ()=>{
+        async function getUser(){
+            const storedUserData = localStorage.getItem('userData');
+                if (storedUserData) {
+                    const parsedUserData: UserLogged = JSON.parse(storedUserData);
+                    setUserData(parsedUserData);
+                }
+                alert(storedUserData);
+        }
+        getUser();
+    },[])
     return(
         <div className='header'>
             <Link to= "/" className='links'>LOGO</Link>
             <div>
+                <label>{userData && <p>Olá, {userData.email}</p>}</label>
+                <Link to= "/carrinho" className='links'><BsCart4 /></Link>
                 <Link to= "/produto" className='links'>Produtos</Link>
                 <Link to= "/favorito" className='links'>Favoritos</Link>           
                 <Link to= "/contato" className='links'>Ir para Contatos</Link>
